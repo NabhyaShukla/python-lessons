368045405defda78806ba361a84d45d080b0ddec
NabhyaShukla <nabhyashukla69@gmail.com>
2026-05-15 21:31:10 +0530

    first commit

---
+++ lesson1/act1.py
@@ -0,0 +1,4 @@
+print("Welcome To Vauid!")
+print("My name is Nabhya.")
+print("I am learning Python through Codingal.")
+print("The Sum of 5 and 3 is :-", 5+3)
---
+++ lesson1/act2.py
@@ -0,0 +1,7 @@
+print("Welcome to the world of Python!")
+print(4)
+print("Hello Python \n")
+# Prints all the keywords
+print("The keywords is Python are :-")
+import keyword
+print(keyword.kwlist)
