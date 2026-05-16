2da2d49d2967e18be12d1105d941a9f2e165eb77
NabhyaShukla <nabhyashukla69@gmail.com>
2026-05-16 21:25:06 +0530

    update

--- lesson2-datatypes/act1.py
+++ lesson2-datatypes/act1.py
@@ -1,7 +1,7 @@
 name = "Nabhya"
 age = 12
 isStudent = True
-weight = 41.8
+weight = 41
 
 print("Name :", name)
 print("Datatype of name is ", type(name).__name__)
@@ -15,3 +15,11 @@ print("Datatype of isStudent is ", type(isStudent).__name__)
 print("Weight :", weight)
 print("Datatype of Weight is ", type(weight).__name__)
 
+# Typecasting
+
+age = str(age)
+print("Datatype of Age after typecasting is :", type(age).__name__)
+
+weight = float(weight)
+print("Datatype of Weight after typecasting is :", type(weight).__name__)
+print("Value of weight after typecasting is :", weight)
