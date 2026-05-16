01ab9c426d108acbefd88d3ab3bb73b962840dd5
NabhyaShukla <nabhyashukla69@gmail.com>
2026-05-16 21:17:18 +0530

    update

---
+++ lesson2-datatypes/act1.py
@@ -0,0 +1,17 @@
+name = "Nabhya"
+age = 12
+isStudent = True
+weight = 41.8
+
+print("Name :", name)
+print("Datatype of name is ", type(name).__name__)
+
+print("Age :", age)
+print("Datatype of age is ", type(age).__name__)
+
+print("Is Student :", isStudent)
+print("Datatype of isStudent is ", type(isStudent).__name__)
+
+print("Weight :", weight)
+print("Datatype of Weight is ", type(weight).__name__)
+
