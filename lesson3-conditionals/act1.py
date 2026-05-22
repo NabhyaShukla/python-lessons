bae46b70fa6de3dda6cf91744e1aeb0b2e7238d1
NabhyaShukla <nabhyashukla69@gmail.com>
2026-05-22 21:07:56 +0530

    update

---
+++ lesson3-conditionals/act1.py
@@ -0,0 +1,6 @@
+# Write a python program first to take an input from the user , then check if it is even or odd
+number = int(input("Enter A number to check : "))
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
