836beeea46c0883bce73cd382a0d19ed991bda1a
NabhyaShukla <nabhyashukla69@gmail.com>
2026-05-23 21:30:50 +0530

    update

--- lesson4-loops/act1.py
+++ lesson4-loops/act1.py
@@ -6,3 +6,5 @@ for i in range(1, 11): # Print multiplication table in limited loops (10 times)
     print(f"{num} x {i} = {num * i}") #f"---" f is for printing the input value by the user
 
 numAdd = int(input("Enter a number to add : "))
+for i in range(1, 11): # Print addition table in limited loops (10 times)
+    print(f"{numAdd} + {i} = {numAdd + i}") #f"---" f is for printing the input value by the user
---
+++ lesson4-loops/act2.py
@@ -0,0 +1,7 @@
+# Star pattern using nested for loop
+
+n = int(input("Enter the number of rows : "))
+for i in range(1, n+1):
+    for j in range(i):
+        print("*", end="")
+    print() # for new line after each row
