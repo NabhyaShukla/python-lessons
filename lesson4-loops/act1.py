7396c29f5c2985c3d65335b92b0ead27e210c4c3
NabhyaShukla <nabhyashukla69@gmail.com>
2026-05-23 21:21:03 +0530

    update

---
+++ lesson4-loops/act1.py
@@ -0,0 +1,8 @@
+#printing a number table 
+
+num = int(input("Enter a number to multiply : "))
+
+for i in range(1, 11): # Print multiplication table in limited loops (10 times)
+    print(f"{num} x {i} = {num * i}") #f"---" f is for printing the input value by the user
+
+numAdd = int(input("Enter a number to add : "))
