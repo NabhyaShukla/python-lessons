994c65cbb807ca9aa054d204c7c718890448dbff
NabhyaShukla <nabhyashukla69@gmail.com>
2026-06-27 21:14:13 +0530

    update

--- lesson13-file-handling/act5.py
+++ lesson13-file-handling/act5.py
@@ -5,9 +5,36 @@
     AND PRINT IN ANOTHER FILE 'WORDS_OUT.TXT'
 
 """
+# input_file = open('word.txt', 'r')
+# output_file = open('words_out.txt', 'w')
 
-file = open("word.txt", "r")
-lines = file.readlines()
+# for line in input_file:
+
+#     name = line.strip()
+#     if len(name) > 4:
+#         output_file.write(name + '\n')
+
+# input_file.close()
+# output_file.close()
+
+long_name =[]
+
+file = open('word.txt', 'r')
+names = file.readlines()
+for name in names:
+    clean_name = name.strip()
+
+    if not clean_name:
+        continue
+
+if len(clean_name) > 4:
+    long_name.append(clean_name)
+
 file.close()
 
-ofile = open("words_out.txt", "w")
+output_file = open('outfile.txt', 'w')
+for name in long_name:
+    output_file.write(name + '\n')
+
+output_file.close()
+print("Done....")
